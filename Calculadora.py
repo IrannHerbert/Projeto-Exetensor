930aee458a0643ce0fa166f7501508534d3b9956
Irann Herbert <irannaraujo8@gmail.com>
2026-06-01 12:33:22 -0300

    Update Menu e Divisão

---
+++ Calculadora.py
@@ -0,0 +1,12 @@
+print("\n  == Menu Calculadora ==  ")
+print("\n  1.Divisão\n  2.Multiplicação\n  3.Subtração\n  4.Soma\n  5.Sair")
+operaçao = float(input("Escolha uma operação matemáticas: "))
+
+if operaçao == 1:
+    divisao1 = float(input("Informe o valor que será dividido: "))
+    divisao2 = float(input("Informe o valor pelo qual o número será dividido: "))
+    resultado_divisao = divisao1 / divisao2
+    print(f"O resultado da sua divisão será: {resultado_divisao} ")
+    
+elif moeda == 5:
+    print("Obrigado por usar nossa calculadora, até breve !")
